9c65a5869713c3955026293038c536156cd6ff0b
Adrian <adrian.talonia11@gmail.com>
2022-03-31 18:20:23 -0600

    add enable conditions

--- junit5_app/src/test/java/com/junit5app/models/AccountTest.java
+++ junit5_app/src/test/java/com/junit5app/models/AccountTest.java
@@ -1,17 +1,47 @@
 package com.junit5app.models;
 
 import com.junit5app.exceptions.InsufficientMoneyException;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
 
 import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS) //with this you can remove statuc
 class AccountTest {
 
+    Account account;
+
+    @BeforeEach
+    void initMethod() {
+        this.account = new Account("Adrian", new BigDecimal("1000.12345"));
+        System.out.println("Init method");
+    }
+
+    @AfterEach
+    void finalMethod() {
+        System.out.println("Ending test method");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Initialize test");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Complete test");
+    }
+
+
     @Test
+    @DisplayName("test user name")
+        //name a method
     void testUser() {
-        Account account = new Account("Adrian", new BigDecimal("1000.4789"));
+        //Account account = new Account("Adrian", new BigDecimal("1000.12345"));
         String expected = "Adrian";
         String real = account.getUser();
         assertEquals(expected, real);
@@ -19,8 +49,10 @@ class AccountTest {
     }
 
     @Test
+    @Disabled
+        //disabled method but that show in the report
     void testMoney() {
-        Account account = new Account("Adrian", new BigDecimal("1000.12345"));
+        //Account account = new Account("Adrian", new BigDecimal("1000.12345"));
         assertEquals(1000.12345, account.getMoney().doubleValue());
         assertFalse(account.getMoney().compareTo(BigDecimal.ZERO) < 0);
     }
@@ -35,7 +67,7 @@ class AccountTest {
 
     @Test
     void testAccountDebit() {
-        Account account = new Account("Adrian", new BigDecimal("1000.12345"));
+        //Account account = new Account("Adrian", new BigDecimal("1000.12345"));
         account.debit(new BigDecimal(100));
         assertNotNull(account.getMoney());
         assertEquals(900, account.getMoney().intValue());
@@ -44,7 +76,7 @@ class AccountTest {
 
     @Test
     void testAccountCredit() {
-        Account account = new Account("Adrian", new BigDecimal("1000.12345"));
+        //Account account = new Account("Adrian", new BigDecimal("1000.12345"));
         account.credit(new BigDecimal(100));
         assertNotNull(account.getMoney());
         assertEquals(1100, account.getMoney().intValue());
@@ -53,7 +85,7 @@ class AccountTest {
 
     @Test
     void insufficientMoneyException() {
-        Account account = new Account("Adrian", new BigDecimal("1000.12345"));
+        //Account account = new Account("Adrian", new BigDecimal("1000.12345"));
         Exception exception = assertThrows(InsufficientMoneyException.class, () -> { //lambda expression
             account.debit(new BigDecimal(1500));
         });
@@ -99,4 +131,63 @@ class AccountTest {
         assertTrue(bank.getAccounts().stream().filter(c -> c.getUser().equals("Adrian")).findFirst().isPresent());
         assertTrue(bank.getAccounts().stream().anyMatch(c -> c.getUser().equals("Adrian")));*/
     }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+        // execute only windows
+    void testOnlyWindows() {
+    }
+
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    void testOnlyLinux() {
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_8)
+    void testOnlyJava8() {
+    }
+
+    @Test
+    void testSystemProperties() {
+        Properties p = System.getProperties();
+        p.forEach((k, v) -> System.out.println(k + ":" + v));
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = "java.version", matches = "11.0.14.1")
+    void testSystemProperty() {
+    }
+
+    @Test
+    @DisabledIfSystemProperty(named = "os.arch", matches = ".*32.*")
+    void testOnly64() {
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = "USERNAME", matches = "adrian")
+    void testUserName() {
+    }
+
+    @Test
+    void getEnv() {
+        Map<String, String> getenv = System.getenv();
+        getenv.forEach((k, v) -> System.out.println(k + " = " + v));
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "JAVA_HOME", matches = ".*java-11.*")
+    void testJavaHome() {
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "ENVIRONMENT", matches = "dev")
+    void testEnv() {
+    }
+
+    @Test
+    @DisabledIfEnvironmentVariable(named = "ENVIRONMENT", matches = "prod")
+    void testEnvProdDisabled() {
+    }
+
 }
